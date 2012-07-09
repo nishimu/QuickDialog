77c184fccad6c2cf37f8e69d80168957ac6f61d6
Toshio Nishimura <nishimura@canisystems.com>
2012-07-10 00:49:43 +0900

    Cell height bugs

--- quickdialog/QLabelElement.m
+++ quickdialog/QLabelElement.m
@@ -84,7 +84,7 @@
         CGSize constraint = CGSizeMake(tableView.frame.size.width-(tableView.root.grouped ? 40.f : 20.f), 20000);
         CGSize  size= [[_value description] sizeWithFont:_font constrainedToSize:constraint lineBreakMode:UILineBreakModeWordWrap];
         CGFloat predictedHeight = size.height + 20.0f;
-        if (self.title!=nil)
+        if (self.title!=nil || !_titleHidden)
             predictedHeight+=30;
         CGFloat height = [super getRowHeightForTableView:tableView];
         return (height >= predictedHeight) ? height : predictedHeight;
